6f43a8766d4ba6aa3b5f601997f1a77834d39be0
IOSF <om2figueroa@gmail.com>
2026-04-16 15:30:55 +0200

    fixin Style grid and list && show genders

--- FRONTEND_COMPONET/src/Components/MovieCardComponent.jsx
+++ FRONTEND_COMPONET/src/Components/MovieCardComponent.jsx
@@ -3,13 +3,38 @@ import React, { useState } from 'react'
 import { apiConfig } from '../Services/ApiConfig';
 import { useNavigate } from 'react-router-dom';
 ////////////////////////////////////////////////////////////////////
-
+///codex añadio array para traducir gender
+const genreMap = {
+  28: "Accion",
+  12: "Aventura",
+  16: "Animacion",
+  35: "Comedia",
+  80: "Crimen",
+  99: "Documental",
+  18: "Drama",
+  10751: "Familiar",
+  14: "Fantasia",
+  36: "Historia",
+  27: "Terror",
+  10402: "Musica",
+  9648: "Misterio",
+  10749: "Romance",
+  878: "Ciencia ficcion",
+  10770: "TV Movie",
+  53: "Thriller",
+  10752: "Belica",
+  37: "Western"
+};
 const MovieCardComponent = (props) => {
   /*NOTA PARA ALEX: OYE AQUI USE EL PROPS ES EL UNICO SITIO DONDE LO HE USADO, PERO PORQUE LO VI CLARO GRACIAS AL VIDEO 56 DE REACT, EJERCICIO LISTUSER, DE OTRA FORMA LO VEO MAS "CLARO" EL CONTEXT, NO SE SI ES BUENA O MALA PRACTICA SI ESTA BIEN O ME ESTOY HACIENDO LA PICHA UN LIO. SI PUDIERAS DARME ALGUN EJERCIO PARA ESOS DOS HOOKS ESTARIA ENCANTADO*/
   ////////////////////////////////////////////////////////////
   const [isHover, setIsHover] = useState(false);
   const { movie, viewMode } = props;
   const navigate = useNavigate()
+   const movieGenres = movie.genre_ids
+  ?.map((id) => genreMap[id])
+  .filter(Boolean)
+  .slice(0, 2) ?? [];///codex añadio map
   const goToDetails = () => { // funcion que realiza la navegacion a details con el parametro de ID
     navigate(`/details/${movie.id}`)
   }
@@ -57,10 +82,16 @@ const MovieCardComponent = (props) => {
           <div className='cardContainer_List-Box1'>
             <h2 className='cardContainer_List-Title'>{movie.title}</h2>
           </div>
-
+ <div className='cardContainer_List-GenresBox'>
+              <span className='cardComponent_Desc-Label'>Generos: </span>
+              <span className='cardContainer_List-GenresText'>
+                {movieGenres.length > 0 ? movieGenres.join(" - ") : "No disponible"}
+              </span>
+            </div>
           <div className='cardContainer_List-Box2' >
             <p className='cardContainer_List-Box2--Text'><span className="cardComponent_Desc-Label">Estreno: </span>{movie.release_date}</p>
             <p className='cardContainer_List-Box2--Text'> <span className='cardComponent_Desc-Label'> Score:</span> {movie.vote_average} / ({movie.vote_count})</p>
+             
             <p className="cardComponent_Desc-Label" style={{ marginTop: "2%" }}>Sinopsis...</p>
           </div>
         </section>
